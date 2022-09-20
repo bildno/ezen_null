f93b6425d60fc27fa330d95bcf630c15430118dd
DANOK <107171748+danokok@users.noreply.github.com>
2022-09-20 17:48:11 +0900

    ㄴㅇㄹㄴㅇ
    ㅇㄹㄴㅇㄹㄴㅇ

--- src/main/java/com/study/springboot/dao/InoticeDao.java
+++ src/main/java/com/study/springboot/dao/InoticeDao.java
@@ -1,5 +1,14 @@
 package com.study.springboot.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.study.springboot.dto.noticeDto;
+
+@Mapper
 public interface InoticeDao {
 
+	public List<noticeDto> notice();
+	
 }
