11807f393494d37b99e3c34c95f90ce80cc901ff
jingyu <gingu33@naver.com>
2022-09-30 14:23:20 +0900

    cc

--- src/main/java/com/study/springboot/Mycontroller.java
+++ src/main/java/com/study/springboot/Mycontroller.java
@@ -777,7 +777,7 @@ public class Mycontroller {
 		List<one2one_answerDto> one2oneanswer_list = one2one_answerService.one2one_answer(member_id);
 
 		model.addAttribute("one2one_list", one2one_list);
-		System.out.println("gg");
+	
 		
 		model.addAttribute("one2oneanswer_list", one2oneanswer_list);
 		
@@ -1183,11 +1183,16 @@ public class Mycontroller {
 
 	/* 공간상세(일반회원) */
 	@RequestMapping("/space_info")
-	public String space_info(Model model) {
+	public String space_info(@RequestParam("hostenter_number") int hostenter_number,
+			Model model) {
+		
+		List<hostenterDto> space_info = hostenterService.space_info(hostenter_number);
 
+		model.addAttribute("space_info",space_info);
 		model.addAttribute("mainPage", "contents/space_info.jsp");
 		return "index";
 	}
+
 	/* ----------------------------------------- */
 
 	/*-------------------------------------------*/
@@ -1387,7 +1392,7 @@ public class Mycontroller {
 					
 				if( upload_url != null ) {
 					if( upload_url.length() > 0 ) {
-						result = hostenter_imgDaoService.hostenter_img_update(upload_url,host_name_);
+						result = 0;
 						System.out.println("업로드 성공!");
 					
 						
