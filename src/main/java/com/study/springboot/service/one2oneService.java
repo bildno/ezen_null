84ffc00e2583d037477864c37c6cfed87e48f4d2
highym <highym@naver.com>
2022-10-04 10:26:29 +0900

    dddddddddddddd

--- src/main/java/com/study/springboot/service/one2oneService.java
+++ src/main/java/com/study/springboot/service/one2oneService.java
@@ -13,19 +13,19 @@ public class one2oneService {
 
 	@Autowired
 	private Ione2oneDao ione2oneDao;
-	
+
 	public List<one2oneDto> one2one_list(String member_id) {
-		
+
 		List<one2oneDto> one2one_list = ione2oneDao.one2one_list(member_id);
-		
+
 		return one2one_list;
 	}
-	
+
 	public int one2one_write(one2oneDto dto) {
 		int result = ione2oneDao.one2one_write(dto);
 		return result;
 	}
-	
+
 //	public List<one2oneDto>ad_one2one_write(String one2one_number) {
 //
 //		List<one2oneDto>ad_one2one_write = ione2oneDao.ad_one2one_write(one2one_number);
@@ -33,11 +33,18 @@ public class one2oneService {
 //		return ad_one2one_write;
 //	}
 
-	public List<one2oneDto>ad_one2one_answer(String one2one_number) {
+	public List<one2oneDto> ad_one2one_answer(String one2one_number) {
 
-		List<one2oneDto>ad_one2one_answer = ione2oneDao.ad_one2one_answer(one2one_number);
+		List<one2oneDto> ad_one2one_answer = ione2oneDao.ad_one2one_answer(one2one_number);
 
 		return ad_one2one_answer;
 	}
-	
+
+	public List<one2oneDto> one2one_search(String search_type, String search_contents) {
+
+		List<one2oneDto> one2one_search = ione2oneDao.one2one_search(search_type, search_contents);
+
+		return one2one_search;
+	}
+
 }
