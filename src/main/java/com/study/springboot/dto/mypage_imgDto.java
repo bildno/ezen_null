759fbcc835a9b56ed912f8f6dac952ab4109e4ed
DANOK <107171748+danokok@users.noreply.github.com>
2022-10-14 17:43:04 +0900

    cgg

---
+++ src/main/java/com/study/springboot/dto/mypage_imgDto.java
@@ -0,0 +1,13 @@
+package com.study.springboot.dto;
+
+import lombok.Data;
+
+@Data
+public class mypage_imgDto {
+
+	private int mypage_img_idx;
+	private String mypage_img_member_id;
+	private String mypage_img;
+	
+	
+}
